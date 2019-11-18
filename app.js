aac9a5cc56fec7e248b1b0621a834346adb9e679
kwlahr <kwlahr@gmail.com>
2019-11-17 18:26:28 -0700

    added Employee class constructor and getName, getId, getRole, and getEmail functions to employee.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const jest = require("jest");
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+var pdf = require('html-pdf');
+var options = {
+    format: 'Letter'
+};
 
 // Sets up the Express App
 // =============================================================
@@ -12,3 +19,6 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Employees DATA
+const employees = [];
