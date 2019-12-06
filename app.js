8836ac9344ffa6da836f122503d9de29451178e2
kwlahr <kwlahr@gmail.com>
2019-12-05 17:42:32 -0700

    added inquirer prompt chain and switch cases

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const path = require("path");
 const axios = require("axios");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
-var pdf = require('html-pdf');
+var pdf = require("html-pdf");
 var options = {
-    format: 'Letter'
+  format: "Letter"
 };
 
 //Module Imports
@@ -16,6 +16,7 @@ const Employee = require("./lib/Employee.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
+const generateHTML = require("./output/generateHTML");
 
 // Sets up the Express App
 // =============================================================
@@ -28,30 +29,141 @@ app.use(express.json());
 
 //Employees DATA
 const employees = [];
+const engineers = [];
+const interns = [];
+const managers = [];
+let id = 0;
+var response;
 
-
-function promptUser() {
-    return inquirer.prompt([{
-            type: "input",
-            name: "name",
-            text: "Enter employee name"
-        },
-        {
-            type: "input",
-            name: "id",
-            text: "Enter employee ID"
-        },
-        {
-            type: "input",
-            name: "email",
-            text: "Enter employee email"
-        },
-        {
-            type: "input",
-            name: "role",
-            text: "Enter employee role"
-        }
-    ]);
+const promptUser = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Name:",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "Email:",
+        name: "email"
+      },
+      {
+        type: "list",
+        name: "position",
+        message: "What's Your Position At The Company?",
+        choices: ["Manager", "Engineer", "Intern"]
+      }
+    ])
+    .then(async function(data) {
+      id = id + 1;
+      switch (data.role) {
+        case "Manager":
+          await inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "Enter office number: ",
+                name: "office"
+              }
+            ])
+            .then(function(res) {
+              const officeNum = res.office;
+              const manager = new Manager(
+                data.name,
+                data.id,
+                data.email,
+                officeNum,
+                "Manager"
+              );
+              employees.push(manager);
+            });
+          break;
+        case "Engineer":
+          await inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "Enter github username: ",
+                name: "github"
+              }
+            ])
+            .then(function(res) {
+              const githubName = res.github;
+              const engineer = new Engineer(
+                data.name,
+                data.id,
+                data.email,
+                githubName,
+                "Engineer"
+              );
+              employees.push(engineer);
+            });
+          break;
+        case "Intern":
+          await inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "Enter school: ",
+                name: "school"
+              }
+            ])
+            .then(function(res) {
+              const internSchool = res.school;
+              const intern = new Intern(
+                data.name,
+                data.id,
+                data.email,
+                internSchool,
+                "Intern"
+              );
+              employees.push(intern);
+            });
+          break;
+      }
+    })
+    .then(function() {
+      addNext();
+    });
 };
 
-promptUser();
+const addNext = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+      name: "add",
+      message: "Would You Like To Add Another Employee?",
+      choices: ["Yes", "No"]
+      }
+    ])
+    .then(function(res) {
+      if (res.add === "Yes") {
+        promptUser();
+      } else {
+        console.log("Done");
+        completedRoster(employees);
+      }
+    });
+};
+
+async function completedRoster(employees){
+    console.log("Success!");
+    console.log(employees);
+    const html = await generateHTML(employees);
+    writeFileAsync("./output/employees.html", html);
+}
+
+function init(){
+  console.log("Please enter employee info")
+  promptUser();
+}
+
+init();
+
+// require("./output/employees.html")(app);
+
+app.listen(PORT, function() {
+  console.log(`App listening on PORT ${PORT}`);
+});
